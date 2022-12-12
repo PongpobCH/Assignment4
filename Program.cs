7e75bcd4845f551796c74b8abb6c9ec8c102823a
pongpob <khlangs456@gmail.com>
2022-12-12 19:59:32 +0700

    work4

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    
+    
+    static void Main()
+    {
+        
+        int i = 0;
+        Stack<string> stack = new Stack<string>(); 
+        string input,input2;
+        input = Console.ReadLine();
+        
+        
+        stack.Push(input);
+
+        input2 = Console.ReadLine();
+
+        if(input2 != "2")
+        {
+            stack.Pop();
+        }
+
+        
+
+        if(input != "exit")
+        {
+            
+            Main();
+            string value = stack.Pop();
+            Console.WriteLine(value);
+            
+        }
+
+        else
+        {
+   
+            
+        }
+
+ 
+        
+    
+    }
+    
+       
+}
